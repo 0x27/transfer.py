43518cbfc026612e44d0a157489c910133ce54c8
David Davidson <g00271860@gmit.ie>
2015-04-15 15:33:59 +0000

    Thanks to Cal Leeming and Raynaldo Riviera and Stack Overflow for solving the upload progress bar issue

--- transfer.py
+++ transfer.py
@@ -8,14 +8,49 @@ from clint.textui import progress
 import sys
 import os
 
+class upload_in_chunks(object):
+    def __init__(self, filename, chunksize=1 << 13):
+        self.filename = filename
+        self.chunksize = chunksize
+        self.totalsize = os.path.getsize(filename)
+        self.readsofar = 0
+
+    def __iter__(self):
+        with open(self.filename, 'rb') as file:
+            while True:
+                data = file.read(self.chunksize)
+                if not data:
+                    sys.stderr.write("\n")
+                    break
+                self.readsofar += len(data)
+                percent = self.readsofar * 1e2 / self.totalsize
+                sys.stderr.write("\r{percent:3.0f}%".format(percent=percent))
+                yield data
+
+    def __len__(self):
+        return self.totalsize
+
+class IterableToFileAdapter(object):
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.length = len(iterable)
+
+    def read(self, size=-1): # TBD: add buffer for `len(data) > size` case
+        return next(self.iterator, b'')
+
+    def __len__(self):
+        return self.length
+
 def upload(file_path):
+    # thanks to http://stackoverflow.com/questions/13909900/progress-of-python-requests-post/13911048#13911048
     try:
         filepath, filename = os.path.split(file_path)
     except Exception, e:
         print "{-} Something has gone horribly wrong! Please report on the github issue tracker with the following backtrace: \n%s" %(e)
     upload_url = "https://transfer.sh/%s" %(filename)
     try:
-        r = requests.put(url=upload_url, data=open(file_path, "rb"))
+        it = upload_in_chunks(file_path, 10)
+        r = requests.put(url=upload_url, data=IterableToFileAdapter(it))
     except Exception, e:
         print "{-} Something has gone horribly wrong! Please report on the github issue tracker with the following backtrace: \n%s" %(e)
     try:
